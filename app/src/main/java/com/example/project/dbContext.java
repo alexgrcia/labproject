bd0ae008ea5b1ae04ee19bdfb73e257a25d45afa
alexgarcia <alexgarcia0592@gmail.com>
2019-11-18 22:34:24 -0600

    creacion de base de acceso y login

--- app/src/main/java/com/example/project/dbContext.java
+++ app/src/main/java/com/example/project/dbContext.java
@@ -24,6 +24,24 @@ public class dbContext extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("insert into accesos(acc_usuario,acc_clave,acc_tipo_usuario) values('encargado','en123',2)");
         sqLiteDatabase.execSQL("insert into accesos(acc_usuario,acc_clave,acc_tipo_usuario) values('instructor','in123',3)");
         Log.i("DATOS_INGRESADOS","Datos ingresados en accesos");
+
+        //creacion de tabla carreras
+        sqLiteDatabase.execSQL("create table carreras(ca_id integer primary key autoincrement, ca_nombre text)");
+
+        // creacion de tabla instructores
+        sqLiteDatabase.execSQL("create table instructores(ins_id integer primary key autoincrement, ins_nombre text, ins_apellidos text, ins_email text, ins_carnet text, ins_id_carrera integer not null, foreign key(ins_id_carrera) references carreras(ca_id))");
+
+        //creacion de la tabla edificios
+        sqLiteDatabase.execSQL("create table edificios(edi_id integer primary key autoincrement, edi_nombre text)");
+
+        //creacion de la tabla laboratorios
+        sqLiteDatabase.execSQL("create table laboratorios(lab_id integer primary key autoincrement, lab_nombre text, lab_correlativo integer, lab_nivel integer, lab_id_edificio integer not null, foreign key(lab_id_edificio) references edificios(edi_id))");
+
+        //creacion de la tabla ciclos
+        sqLiteDatabase.execSQL("create table ciclos(ci_id integer primary key autoincrement, ci_codigo text,ci_fecha_inicio text, ci_fecha_fin text, ci_estado)");
+
+        // creacion de la tabla empleados
+
     }
 
     @Override
